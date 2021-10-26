966433f3c16d8f2ff7dd574f95d8ddfed79f2e87
izzatulazizah <45443539+izzatulazizah@users.noreply.github.com>
2021-10-26 09:46:04 +0700

    Update matriks_representation.py

--- matriks_representation.py
+++ matriks_representation.py
@@ -1,40 +1,43 @@
 import numpy as np
-def c_stochastic(adj_list):
+def create_stochastic(adj_list):
   n = len(adj_list)
-  matrix = np.zeros((n, n))
-  for col, line in enumerate(adj_list):
+  matrix = np.zeros((n, n)) 
+  for row, line in enumerate(adj_list):
     if len(line) > 0:
       for index in line:
-        matrix[index][col] = 1/(len(line))
+        matrix[index][row] = 1/(len(line))
     else:
       for index in range(n):
-        matrix[index][col] = 0
+        matrix[index][row] = 1/n
   return matrix
-  
+
+def pg(matriks, jml_brs, max_iter):
   iter = 0
-max_iter = 6
-t = 1
-d = 0.85
-matriks = np.array(stochastic)
+  t = 1
+  matriks = np.array(stochastic)
 
-matriks2 = np.array([[d],[d],[d],[d]])
+  matriks2 = ([0]*jml_brs)
+  for i in range(jml_brs): 
+      matriks2[i] = [1/jml_brs]*1
+  print(matriks2)
 
-while iter < max_iter:
-  print('\nIterasi ', iter)
-  hasil = []
-  matriks1 = matriks**t
-  
-  for i in range(len(matriks1)):
-    baris = []
-    for k in range(len(matriks2[0])):
-       val = 0
-       for j in range(len(matriks1[i])):
-          val += matriks1[i][j] * matriks2[j][k]
-       baris.append(val)
-    hasil.append(baris)
+  while iter < max_iter:
+    print('\nIterasi ', iter)
+    hasil = []
+    matriks1 = matriks**t
+    
+    for i in range(len(matriks1)):
+      baris = []
+      for k in range(len(matriks2[0])):
+        val = 0
+        for j in range(len(matriks1[i])):
+            val += matriks1[i][j] * matriks2[j][k]
+        baris.append(val)
+      hasil.append(baris)
 
-  print("PR",iter, '=')
-  for baris in hasil:
-      print(baris)
-  iter += 1
-  t += 1 
+    print("PR",iter, '=')
+    for baris in hasil:
+        print(baris)
+    iter += 1
+    t += 1 
+  return baris
